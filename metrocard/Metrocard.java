52538522a99f69f50c1299d119f6515348794478
Carol Chan <carolc4@nycstudents.net>
2022-02-20 15:54:00 -0500

    commenting

--- metrocard/Metrocard.java
+++ metrocard/Metrocard.java
@@ -7,6 +7,7 @@ public class Metrocard{
   private String time;
 
 
+//CONSTRUCTORS:
 
 //unlimited
 public Metrocard(boolean e, int d){
@@ -20,51 +21,55 @@ public Metrocard(boolean e, int d){
     this.price = 34;
   }
 }
+
 //regular (2.75 per ride)
 public Metrocard(double p){
   this.price = p;
   this.unlimited = false;
 }
+
 //student
 public Metrocard(int dOW, String time){
   //5am - 8pm time limit
   //only on weekdays
   //time limit between swipes ~20 mins
+  //3 swipes per day
 }
 
 
-
-
-
-
-
-
+//METHODS:
 public double getPrice(){
   return this.price;
-}
+}//end getPrice
 
-
+//check if card is expired
 public void isExpired(){
   if(this.expiration){
     System.out.println("expired");
   }
-}
+}//end isExpired
+
+//change the expiration to true or false
 public void setE(boolean e){
-  this.expiration = e; 
-}
+  this.expiration = e;
+}//end setE
 
+//add money to a regular metrocard
+public void addMoney(double add){
+  this.price += add;
+}//end addMoney
 
-public double addMoney(double add){
-  return this.price + add;
-
-}
-
+//if regular metrocard return original price-2.75 but if unlimited returns the original price
 public double swipe(){
-  if(this.unlimited == false){
-    this.price -= 2.75;
+  if(this.unlimited){
+    return this.price;
   }
-  return this.price;
-}
+  else if(this.unlimited == false){
+    this.price -= 2.75;
+    return this.price;
+  }
+  return this.price;//return for if student metrocard
+}//end swipe
 
 
 }//end class
--- metrocard/tester.java
+++ metrocard/tester.java
@@ -1,10 +1,8 @@
-public class tester{
+public class tester {
   public static void main(String[] args){
     Metrocard m1 = new Metrocard(30.75);
-    m1.swipe();
+    m1.addMoney(20);
     System.out.println(m1.getPrice());
 
   }
-
-
 }
